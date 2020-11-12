eba957b20de1cacb76e8416aac0199766faa409d
David Egerton <david.egertonsmith@gmail.com>
2020-11-12 12:25:14 +0000

    home page route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/pdf'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to "pages#home"
 end
